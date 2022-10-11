0ca4fc78b9d3fdc0e63e2ce565bd977c3fc7635e
Jayson <leejayson01@gmail.com>
2022-10-10 23:31:52 -0500

    Finish game logic, left to design UI with CSS

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("Hello World")
 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const restartBtn = document.querySelector("#restart");
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+// rockBtn.addEventListener('click', function (e) {
+//     console.log(e.target);
+//     e.target.style.background = 'blue';
+// });
+
+
+
+
 /*
 randomly return either 'Rock', 'Paper', or 'Scissors'
 first have variables 
@@ -34,6 +49,7 @@ let rock = "rock",
     }
 }
 
+
 /*
 Play a round of rock-paper-scissors
 Player must input either rock, paper, scissors, case-insensitive
@@ -48,56 +64,153 @@ function playRound(playerSelection, computerSelection){
     let lostMessage = "You lost! " + computerSelection + " beats " + playerDecision;
 
 
+
     if(playerDecision == "rock" && computerSelection == "rock"){
         return tieMessage;
     }
     else if(playerDecision == "rock" && computerSelection == "paper"){
+        computerScore++;
         return lostMessage;
     }
     else if(playerDecision == "rock" && computerSelection == "scissors"){
+        playerScore++;
         return winningMessage;
     }
     else if(playerDecision == "paper" && computerSelection == "rock"){
+        playerScore++;
         return winningMessage;
     }
     else if(playerDecision == "paper" && computerSelection == "paper"){
         return tieMessage;
     }
     else if(playerDecision == "paper" && computerSelection == "scissors"){
+        computerScore++;
         return lostMessage;
     }
     else if(playerDecision == "scissors" && computerSelection == "rock"){
+        computerScore++;
         return lostMessage;
     }
     else if(playerDecision == "scissors" && computerSelection == "paper"){
+        playerScore++;
         return winningMessage;
     }
     else if(playerDecision == "scissors" && computerSelection == "scissors"){
         return tieMessage;
-    }
-        
+    }    
 }
+
+function getMessageRock(){
+    let text = playRound('rock',getComputerChoice());
+    document.getElementById("message").innerText = text;
+}
+function getMessagePaper(){
+    let text = playRound('paper',getComputerChoice());
+    document.getElementById("message").innerText = text;
+}
+function getMessageScissors(){
+    let text = playRound('scissors',getComputerChoice());
+    document.getElementById("message").innerText = text;
+}
+
+function displayResult(){
+    let result = "Player : " + playerScore + ", Computer:  " + computerScore;
+    document.getElementById("result").innerText = result;
+}
+
+function finishGameResult(){
+    let restartText = "Would you like to play again?"
+    if(playerScore == 5){
+        let finishResult = "Good game, you won"
+        document.getElementById("result").innerText = finishResult;
+        
+    }
+    else{
+        let finishResult = "You lost, to a bot T-T"
+        document.getElementById("result").innerText = finishResult;
+    }
+    document.getElementById("restartText").innerText = restartText;
+
+}
+
+displayResult();
+restartBtn.hidden = true;
+
+
+rockBtn.addEventListener('click', () => {
+    getMessageRock();
+    displayResult();
+    if(playerScore >= 5 || computerScore >= 5){
+        console.log(playerScore);
+        finishGameResult();
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        restartBtn.hidden = false;
+    }
+});
+
+paperBtn.addEventListener('click', () => {
+    getMessagePaper();
+    displayResult();
+    if(playerScore >= 5 || computerScore >= 5){
+        console.log(playerScore);
+        finishGameResult();
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        restartBtn.hidden = false;
+    
+    }
+});
+
+scissorsBtn.addEventListener('click', () => {
+    getMessageScissors();
+    displayResult();
+    if(playerScore >= 5 || computerScore >= 5){
+        console.log(playerScore);
+        finishGameResult();
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        restartBtn.hidden = false;
+    }
+});
+restartBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    displayResult();
+    document.getElementById("message").innerText = "";
+    document.getElementById("restartText").innerText = "";
+    restartBtn.hidden = true;
+});
 
 
 /*
  Runs the game in a best of 5 while keeping the score. 
  Reports the winner or loser at the end
 */
-function game(){
+// function game(){
 
-    for(let i = 0; i < 5; i++){
-        let playerInput = prompt("Input rock, paper, or scissors");
-        let text = "Please try again";
-        if (playerInput == "rock" || playerInput == "paper" || playerInput == "scissors"){
-            console.log(playRound(playerInput, getComputerChoice()));
-        }
-        else{
-            return text;
-        }
+//     console.log("hi");
 
-    }
-}
+//    for(let i = 0; i < 5; i++){
+//         let playerInput = prompt("Input rock, paper, or scissors");
+//         let text = "Please try again";
+//         if (playerInput == "rock" || playerInput == "paper" || playerInput == "scissors"){
+//             console.log(playRound(playerInput, getComputerChoice()));
+//         }
+//         else{
+//             return text;
+//         }
 
-game();
+//     }
+// }
+//game();
 
 
+
+//rockBtn.addEventListener("click" , playRound(e));
